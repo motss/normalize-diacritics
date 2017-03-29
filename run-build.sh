6574491142a9d5fe4b2d87979c5da542bc50fd49
Rong Sen Ng <wes.ngrongsen@gmail.com>
2017-03-29 21:22:57 +0800

    :shell: Updated build script

--- run-build.sh
+++ run-build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-clear; printf "Running run-build script...\n"
+clear; printf "Running %s script...\n" "$(basename "$0" .sh)"
 
 has_unused_directories=false
 directories="node_modules/"
