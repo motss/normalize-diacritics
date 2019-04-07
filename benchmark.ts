6c91301ddb51e994406185d910134f32a002b493
Rong Sen Ng (motss) <wes.ngrongsen@gmail.com>
2019-04-07 20:46:44 +0800

    ✨ src: Add benchmark script

---
+++ benchmark.ts
@@ -0,0 +1,16 @@
+import { bench, runBenchmarks } from 'https://deno.land/x/std/testing/bench.ts';
+import { normalize } from './mod.ts';
+
+bench({
+  name: 'runs3ForNormalizeX1e6s',
+  runs: 3,
+  async func(b) {
+    b.start();
+    for (let i = 0; i < 1e3; i += 1) {
+      await normalize(`söme stüff with áccènts ${Math.random().toString(16).slice(-7)}`);
+    }
+    b.stop();
+  },
+});
+
+runBenchmarks();
