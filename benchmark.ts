dc581b45df83cdf174b4dca39a10a51e346b6517
Rong Sen Ng (motss) <wes.ngrongsen@gmail.com>
2019-04-07 20:49:30 +0800

    💩 src: Format code

--- benchmark.ts
+++ benchmark.ts
@@ -1,16 +1,20 @@
-import { bench, runBenchmarks } from 'https://deno.land/x/std/testing/bench.ts';
-import { normalize } from './mod.ts';
+import { bench, runBenchmarks } from "https://deno.land/x/std/testing/bench.ts";
+import { normalize } from "./mod.ts";
 
 bench({
-  name: 'runs3ForNormalizeX1e6s',
+  name: "runs3ForNormalizeX1e6s",
   runs: 3,
   async func(b) {
     b.start();
     for (let i = 0; i < 1e3; i += 1) {
-      await normalize(`söme stüff with áccènts ${Math.random().toString(16).slice(-7)}`);
+      await normalize(
+        `söme stüff with áccènts ${Math.random()
+          .toString(16)
+          .slice(-7)}`
+      );
     }
     b.stop();
-  },
+  }
 });
 
 runBenchmarks();
